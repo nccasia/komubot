f62f6b0a0da16e96ee468cc915fceb6e80734204
Nguyen Tran Nhan <nguyentran@ncc.asia>
2022-01-11 12:56:51 +0700

    add tx8

---
+++ models/tx8Data.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+
+const tx8db = new mongoose.Schema({
+     messageId: { type: String, required: true },
+     userId: {type: String, required: true },
+     tx8number: { type: Number, required: true },
+     status: { type: String, required: true },
+     createdTimestamp: { type: mongoose.Decimal128, required: true }
+})
+
+const tx8Data = module.exports = mongoose.model('komu_tx8', tx8db)
