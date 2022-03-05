43c1124b5cd9fe284132f838c2f4ba36751ccfc1
SinhHai <hai.nguyensinh.ncc@gmail.com>
2022-03-05 17:35:31 +0700

    fix dmmessage

--- util/dmmessage.js
+++ util/dmmessage.js
@@ -4,6 +4,12 @@ const userData = require('../models/userData');
 const msgData = require('../models/msgData');
 const API_TOKEN = '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';
 const API_URL = 'http://172.16.100.111:3000/webhooks/rest/webhook';
+const userStatus = require('../commands/komu/user_status');
+const toggleActivation = require('../commands/komu/toggleActivation');
+const sync_role = require('../commands/komu/sync_role');
+const ticket = require('../slashcommands/ticket');
+const keep = require('../slashcommands/keep');
+const wiki = require('../slashcommands/wiki');
 
 const getMessageAI = async (url, sender, message, token) => {
   try {
@@ -21,88 +27,110 @@ const getMessageAI = async (url, sender, message, token) => {
   }
 };
 
-const dmmessage = async (message) => {
+const dmmessage = async (message, client) => {
   try {
-    const channelId = message.channelId;
-    const createdTimestamp = message.createdTimestamp;
-    const authorId = message.author.id;
-    const content = message.content;
+    const args = message.content.split(' ').splice(1, 2);
+    switch (message.content.slice(0, 5)) {
+      case '*user':
+        const status = userStatus.execute(message, args, client);
+        break;
+      case '*togg':
+        const toggle = toggleActivation.execute(message, args);
+        break;
+      case '*sync':
+        const sync = sync_role.execute(message, args, client);
+        break;
+      // case '/tick':
+      //   const slashTicket = ticket.execute(message, client);
+      //   break;
+      // case '/keep':
+      //   const keep = ticket.execute(message, client);
+      //   break;
+      // case '/wiki':
+      //   const wiki = ticket.execute(message, client);
+      //   break;
+      default:
+        const channelId = message.channelId;
+        const createdTimestamp = message.createdTimestamp;
+        const authorId = message.author.id;
+        const content = message.content;
 
-    const data = await conversationData
-      .findOne({
-        channelId: channelId,
-        authorId: authorId,
-        createdTimestamp: { $gte: Date.now() - 20000 },
-      })
-      .catch(console.log);
+        const data = await conversationData
+          .findOne({
+            channelId: channelId,
+            authorId: authorId,
+            createdTimestamp: { $gte: Date.now() - 20000 },
+          })
+          .catch(console.log);
 
-    if (!authorId || !content) return;
-    const newMsg = new msgData({
-      channelId: message.channelId,
-      guildId: message.guildId,
-      id: message.id,
-      createdTimestamp: message.createdTimestamp,
-      type: message.type,
-      system: message.system,
-      content: message.content,
-      author: message.author.id,
-      pinned: message.pinned,
-      tts: message.tts,
-      nonce: message.nonce,
-      webhookId: message.webhookId,
-      applicationId: message.applicationId,
-      flags: message.flags,
-    });
-    const newMsgAfter = await newMsg.save();
+        if (!authorId || !content) return;
+        const newMsg = new msgData({
+          channelId: message.channelId,
+          guildId: message.guildId,
+          id: message.id,
+          createdTimestamp: message.createdTimestamp,
+          type: message.type,
+          system: message.system,
+          content: message.content,
+          author: message.author.id,
+          pinned: message.pinned,
+          tts: message.tts,
+          nonce: message.nonce,
+          webhookId: message.webhookId,
+          applicationId: message.applicationId,
+          flags: message.flags,
+        });
+        const newMsgAfter = await newMsg.save();
 
-    await userData.updateOne(
-      { id: authorId, deactive: { $ne: true } },
-      {
-        last_message_id: newMsgAfter.id,
-        last_bot_message_id: '',
-      }
-    );
-
-    const res = await getMessageAI(
-      API_URL,
-      message.author.username,
-      `${content}`,
-      API_TOKEN
-    );
-
-    if (res && res.data && res.data.length) {
-      res.data.map((item) => {
-        return message.channel.send(item.text).catch(console.log);
-      });
-    } else {
-      message.channel.send(
-        "Very busy, too much work today. I'm so tired. BRB."
-      );
-      return;
-    }
-
-    if (data) {
-      await conversationData
-        .updateOne(
-          { _id: data._id },
+        await userData.updateOne(
+          { id: authorId, deactive: { $ne: true } },
           {
+            last_message_id: newMsgAfter.id,
+            last_bot_message_id: '',
+          }
+        );
+
+        const res = await getMessageAI(
+          API_URL,
+          message.author.username,
+          `${content}`,
+          API_TOKEN
+        );
+
+        if (res && res.data && res.data.length) {
+          res.data.map((item) => {
+            return message.channel.send(item.text).catch(console.log);
+          });
+        } else {
+          message.channel.send(
+            "Very busy, too much work today. I'm so tired. BRB."
+          );
+          return;
+        }
+
+        if (data) {
+          await conversationData
+            .updateOne(
+              { _id: data._id },
+              {
+                past_user_inputs: [content],
+                generated_responses: res.data.map((item) => item.text),
+                updatedTimestamp: createdTimestamp,
+              }
+            )
+            .catch(console.log);
+        } else {
+          await new conversationData({
+            channelId: channelId,
+            authorId: authorId,
+            createdTimestamp: createdTimestamp,
+            updatedTimestamp: createdTimestamp,
             past_user_inputs: [content],
             generated_responses: res.data.map((item) => item.text),
-            updatedTimestamp: createdTimestamp,
-          }
-        )
-        .catch(console.log);
-    } else {
-      await new conversationData({
-        channelId: channelId,
-        authorId: authorId,
-        createdTimestamp: createdTimestamp,
-        updatedTimestamp: createdTimestamp,
-        past_user_inputs: [content],
-        generated_responses: res.data.map((item) => item.text),
-      })
-        .save()
-        .catch(console.log);
+          })
+            .save()
+            .catch(console.log);
+        }
     }
   } catch (error) {
     console.error(error);
