04f2206574a29cc2489471507741c6281672e1ae
thanhle <thanh.levan.ncc@gmail.com>
2022-03-07 18:57:54 +0700

    fix meeting

--- commands/komu/meeting.js
+++ commands/komu/meeting.js
@@ -76,10 +76,10 @@ module.exports = {
       } else {
         const task = args.slice(0, 1).join(' ');
         const datetime = args.slice(1, 3).join(' ');
-        let repeat = args.slice(3, args.length).join(' ');
+        let repeat = args.slice(3, 4).join(' ');
+        let repeatTime = args.slice(4, args.length).join(' ');
         const checkDate = args.slice(1, 2).join(' ');
         const checkTime = args.slice(2, 3).join(' ');
-
         if (
           !/^(((0[1-9]|[12]\d|3[01])\/(0[13578]|1[02])\/((19|[2-9]\d)\d{2}))|((0[1-9]|[12]\d|30)\/(0[13456789]|1[012])\/((19|[2-9]\d)\d{2}))|((0[1-9]|1\d|2[0-8])\/02\/((19|[2-9]\d)\d{2}))|(29\/02\/((1[6-9]|[2-9]\d)(0[48]|[2468][048]|[13579][26])|(([1][26]|[2468][048]|[3579][26])00))))$/.test(
             checkDate
@@ -108,6 +108,7 @@ module.exports = {
           task: task,
           createdTimestamp: timestamp,
           repeat: repeat,
+          repeatTime: repeatTime,
         }).save();
         message.reply({ content: '`✅` Meeting saved.', ephemeral: true });
       }
