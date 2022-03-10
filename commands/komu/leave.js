4ffc14abdaf3ba3571a8d09295cc3e9580291630
le duc anh <anh.leduc1@ncc.asia>
2022-03-10 14:52:39 +0700

    add leave

---
+++ commands/komu/leave.js
@@ -0,0 +1,32 @@
+const leaveData = require('../../models/leaveData');
+module.exports = {
+  name: 'leave',
+  description: 'Leave Work',
+  cat: 'komu',
+  async execute(message, args, client) {
+    try {
+      if (!args[0] || !args[1]) {
+        return message.reply('```' + '*leave minute reason ' + '```');
+      }
+      const minute =
+        !isNaN(parseFloat(args[0])) && !isNaN(args[0] - 0) && parseInt(args[0]);
+
+      if (minute === false) {
+        return message.reply('Minute must be a number');
+      }
+      const reason = args.slice(1, args.length).join(' ');
+
+      const newLeave = new leaveData({
+        userid: message.author.id,
+        minute,
+        reason,
+      });
+
+      await newLeave.save();
+
+      return message.reply('Leave saved');
+    } catch (err) {
+      console.log(err);
+    }
+  },
+};
---
+++ models/leaveData.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const leavedb = new mongoose.Schema({
+  userid: { type: String, required: true },
+  reason: { type: String, required: true },
+  minute: { type: Number, required: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('komu_leave', leavedb);
