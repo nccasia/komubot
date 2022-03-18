c39f1661212c113c98f077e03ded1488c3a12619
le duc anh <anh.leduc1@ncc.asia>
2022-03-18 12:00:27 +0700

    replay ncc 8

---
+++ commands/komu/ncc8.js
@@ -0,0 +1,22 @@
+const audioPlayer = require('../../util/audioPlayer');
+const checkNumber = (string) =>
+  !isNaN(parseFloat(string)) && !isNaN(string - 0) && parseInt(string);
+
+module.exports = {
+  name: 'ncc8',
+  description: 'Ncc8',
+  cat: 'komu',
+  async execute(message, args, client) {
+    try {
+      if (args[0] !== 'play' || args[0] === 'help' || !args[1]) {
+        return message.channel.send('```' + '*ncc8 play episode' + '```');
+      }
+      if (!checkNumber(args[1])) {
+        return message.channel.send('```' + 'episode must be number' + '```');
+      }
+      await audioPlayer(client, message, args[1]);
+    } catch (err) {
+      console.log(err);
+    }
+  },
+};
--- models/uploadFileData.js
+++ models/uploadFileData.js
@@ -4,6 +4,7 @@ const uploadFiledb = new mongoose.Schema({
   filePath: { type: String, required: true },
   fileName: { type: String, required: true },
   createdTimestamp: { type: mongoose.Decimal128, required: false },
+  episode: { type: Number, require: 'true', unique: true },
 });
 
 module.exports = mongoose.model('komu_uploadFile', uploadFiledb);
