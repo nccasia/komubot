057d78192c5a903f75520de4d1df1291ab461a8c
thanh <thanhle19071999@gmail.com>
2022-05-19 18:52:05 +0700

    add *thongbao and update *daily

---
+++ commands/komu/notification.js
@@ -0,0 +1,62 @@
+const { sendErrorToDevTest } = require('../../util/komubotrest.js');
+const axios = require('axios');
+const userData = require('../../models/userData');
+
+module.exports = {
+  name: 'thongbao',
+  description: 'Thong bao',
+  cat: 'komu',
+  async execute(message, args, client) {
+    try {
+      const authorId = message.author.id;
+      const noti = args.join(' ');
+      const checkRoleHr = await userData.find({
+        id: authorId,
+        deactive: { $ne: true },
+        roles_discord: 'HR',
+      });
+
+      if (checkRoleHr.length === 0) {
+        return message
+          .reply({
+            content:
+              '```You do not have permission to execute this command!```',
+            ephemeral: true,
+          })
+          .catch((err) => {
+            sendErrorToDevTest(client, authorId, err);
+          });
+      }
+
+      if (!noti || noti == undefined) {
+        return message
+          .reply({
+            content: '```please add your text```',
+            ephemeral: true,
+          })
+          .catch((err) => {
+            sendErrorToDevTest(client, authorId, err);
+          });
+      }
+
+      await axios.post(
+        client.config.noti.api_url_quickNews,
+        {
+          content: noti,
+        },
+        {
+          headers: {
+            securityCode: process.env.QUICKNEWS_KEY_SECRET,
+          },
+        }
+      );
+      message
+        .reply({ content: '`✅` Notification saved.', ephemeral: true })
+        .catch((err) => {
+          sendErrorToDevTest(client, authorId, err);
+        });
+    } catch (err) {
+      console.log(err);
+    }
+  },
+};
