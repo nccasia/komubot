a91685db6c03f94e442568e466150b0d145a4ea4
thanh <thanhle19071999@gmail.com>
2022-05-24 18:16:17 +0000

    fix time *daily

--- commands/komu/daily.js
+++ commands/komu/daily.js
@@ -10,18 +10,20 @@ function checkTimeSheet() {
   const time = new Date();
   const cur = new Date();
   const timezone = time.getTimezoneOffset() / -60;
+
   const fisrtTimeMorning = new Date(
-    setTime(time, 7 + timezone, 30, 0, 0)
+    setTime(time, 0 + timezone, 30, 0, 0)
   ).getTime();
   const lastTimeMorning = new Date(
-    setTime(time, 9 + timezone, 31, 0, 0)
+    setTime(time, 2 + timezone, 31, 0, 0)
   ).getTime();
   const fisrtTimeAfternoon = new Date(
-    setTime(time, 12 + timezone, 0, 0, 0)
+    setTime(time, 5 + timezone, 0, 0, 0)
   ).getTime();
   const lastTimeAfternoon = new Date(
-    setTime(time, 14 + timezone, 1, 0, 0)
+    setTime(time, 7 + timezone, 1, 0, 0)
   ).getTime();
+
   if (
     (cur.getTime() >= fisrtTimeMorning && cur.getTime() <= lastTimeMorning) ||
     (cur.getTime() >= fisrtTimeAfternoon && cur.getTime() <= lastTimeAfternoon)
@@ -50,18 +52,6 @@ module.exports = {
           });
       }
 
-      if (!checkTimeSheet()) {
-        return message
-          .reply({
-            content:
-              '```✅ Daily saved. (Invalid daily time frame. Please daily at 7h30-9h30, 12h-14h. WFH not daily 20k/day.)```',
-            ephemeral: true,
-          })
-          .catch((err) => {
-            sendErrorToDevTest(client, authorId, err);
-          });
-      }
-
       await new dailyData({
         userid: message.author.id,
         email:
@@ -74,11 +64,23 @@ module.exports = {
       })
         .save()
         .catch((err) => console.log(err));
-      message
-        .reply({ content: '`✅` Daily saved.', ephemeral: true })
-        .catch((err) => {
-          sendErrorToDevTest(client, authorId, err);
-        });
+      if (!checkTimeSheet()) {
+        message
+          .reply({
+            content:
+              '```✅ Daily saved. (Invalid daily time frame. Please daily at 7h30-9h30, 12h-14h. WFH not daily 20k/day.)```',
+            ephemeral: true,
+          })
+          .catch((err) => {
+            sendErrorToDevTest(client, authorId, err);
+          });
+      } else {
+        message
+          .reply({ content: '`✅` Daily saved.', ephemeral: true })
+          .catch((err) => {
+            sendErrorToDevTest(client, authorId, err);
+          });
+      }
     } catch (err) {
       console.log(err);
     }
