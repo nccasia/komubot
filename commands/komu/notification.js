258e1dd4215ebc2e0cf7297021d52cc22613293a
hoang dang vinh <vinh.hoangdang@ncc.asia>
2022-06-03 17:05:38 +0700

    edit noti

--- commands/komu/notification.js
+++ commands/komu/notification.js
@@ -1,6 +1,7 @@
 const { sendErrorToDevTest } = require('../../util/komubotrest.js');
 const axios = require('axios');
 const userData = require('../../models/userData');
+const { MessageActionRow, MessageButton, MessageEmbed } = require('discord.js');
 
 module.exports = {
   name: 'thongbao',
@@ -15,8 +16,9 @@ module.exports = {
         deactive: { $ne: true },
         $or: [
           { roles_discord: { $all: ['ADMIN'] } },
-          { roles_discord: { $all: ['HR'] } },
+          { roles_discord: { $all: ['INTERN'] } },
         ],
+        // roles_discord: 'INTERN',
       });
 
       if (checkRole.length === 0) {
@@ -50,6 +52,7 @@ module.exports = {
         {
           headers: {
             securityCode: process.env.IMS_KEY_SECRET,
+            // securityCode: process.env.QUICKNEWS_KEY_SECRET,
           },
         }
       );
@@ -66,10 +69,19 @@ module.exports = {
         '921652536933499002',
         '969511102885019688',
         '921239541388554240',
+        // // '922445995420315701',
+        // '968696449128869968',
+        // // '978500975637725204',
       ];
+
       fetchChannel.map(async (channel) => {
         const userDiscord = await client.channels.fetch(channel);
-        userDiscord.send(`${noti}`);
+        if (message.attachments.first().url)
+          userDiscord.send({
+            content: `${noti}`,
+            files: [message.attachments.first().url],
+          });
+        else userDiscord.send(`${noti} `);
       });
     } catch (err) {
       console.log(err);
