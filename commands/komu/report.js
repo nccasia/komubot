65b6a20c45d126f8920f456f56edb787b0724dec
SinhHai <hai.nguyensinh.ncc@gmail.com>
2022-06-14 10:02:43 +0700

    add report daily datetime

--- commands/komu/report.js
+++ commands/komu/report.js
@@ -40,10 +40,27 @@ module.exports = {
   async execute(message, args, client, guildDB) {
     try {
       if (args[0] === 'daily') {
-        await reportDaily(null, message, args, client, guildDB);
-      } else if (args[0] === 'weekly') {
-        for (const day of getTimeWeekMondayToFriday(new Date().getDay())) {
-          await reportDaily(day, message, args, client, guildDB);
+        if (args[1] === 'weekly') {
+          for (const day of getTimeWeekMondayToFriday(new Date().getDay())) {
+            await reportDaily(day, message, args, client, guildDB);
+          }
+        } else if (args[1]) {
+          const day = args[1].slice(0, 2);
+          const month = args[1].slice(3, 5);
+          const year = args[1].slice(6);
+
+          const fomat = `${month}/${day}/${year}`;
+          const dateTime = new Date(fomat);
+          if (
+            !/^(((0[1-9]|[12]\d|3[01])\/(0[13578]|1[02])\/((19|[2-9]\d)\d{2}))|((0[1-9]|[12]\d|30)\/(0[13456789]|1[012])\/((19|[2-9]\d)\d{2}))|((0[1-9]|1\d|2[0-8])\/02\/((19|[2-9]\d)\d{2}))|(29\/02\/((1[6-9]|[2-9]\d)(0[48]|[2468][048]|[13579][26])|(([1][26]|[2468][048]|[3579][26])00))))$/.test(
+              args[1]
+            )
+          ) {
+            return;
+          }
+          await reportDaily(dateTime, message, args, client, guildDB);
+        } else {
+          await reportDaily(null, message, args, client, guildDB);
         }
       } else if (args[0] === 'mention') {
         await reportMention(message, args, client, guildDB);
