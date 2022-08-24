31aeccd0c5c89600b509260d5df954a2f2e8df0e
Doan Ngoc Ha <doanha2111999@gmail.com>
2022-08-24 17:41:56 +0700

    get userName cancel order

--- commands/komu/order.js
+++ commands/komu/order.js
@@ -1,4 +1,3 @@
-const axios = require('axios');
 const orderData = require('../../models/orderData');
 
 module.exports = {
@@ -7,24 +6,47 @@ module.exports = {
   cat: 'komu',
   async execute(message, args) {
     try {
-      const list = args.slice(0, args.length).join(' ');
       const channel = message.channelId;
       const author = message.author.id;
       const username = message.author.username;
-
-      await new orderData({
-        channelId: channel,
-        userId: author,
-        username: username,
-        menu: list,
-        createdTimestamp: Date.now(),
-      })
-        .save()
-        .catch((err) => console.log(err));
-      message.reply({
-        content: '`✅` Bạn đã đặt đơn!!!',
-        ephemeral: true,
-      });
+      if (args[0] === 'cancel') {
+        const userCancel = await orderData.find({
+          userId: author,
+          isCancel: { $ne: true },
+          channelId: channel,
+          username: username,
+        });
+        userCancel.map(async (item) => {
+          console.log(item);
+          await orderData.updateOne(
+            {
+              _id: item._id,
+            },
+            { $set: { isCancel: true } }
+            // { isCancel: true }
+          );
+        });
+        message.reply({
+          content: 'Bạn đã cancel đơn đặt hàng',
+          ephemeral: true,
+        });
+      } else {
+        const list = args.slice(0, args.length).join(' ');
+        await new orderData({
+          channelId: channel,
+          userId: author,
+          username: username,
+          menu: list,
+          createdTimestamp: Date.now(),
+          isCancel: false,
+        })
+          .save()
+          .catch((err) => console.log(err));
+        message.reply({
+          content: '`✅` Bạn đã đặt đơn!!!',
+          ephemeral: true,
+        });
+      }
     } catch (error) {
       console.log(error);
     }
