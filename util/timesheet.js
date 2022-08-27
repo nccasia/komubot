ead50a5aae2b7fe523745b93ef5ad86dd0509899
Minh Luc Van <minh.lucvan@ncc.asia>
2022-08-27 17:15:11 +0700

    reafactor daily message

--- util/timesheet.js
+++ util/timesheet.js
@@ -1,83 +1,89 @@
+const parseDuration = require('parse-duration')
+const chrono = require('chrono-node')
+const axios = require('axios')
+
 let DEBUG = false
-const LOG_TIMESHEET_REQUIRED_FILEDS = ["project", "note", "workingTime", "task"]
-const TIMESHEET_MESSAGE_HELP =
-   '```' +
-   'Please log timesheet follow this template' +
-   '\n' +
-   '*logTimesheet dd/mm/yyyy' +
-   '\n' +
-   '- project:' +
-   '\n' +
-   '- note:' +
-   '\n' +
-   '- workingTime: ' +
-   '\n' +
-   '- task: ' +
-   '```';
-const checkLogTimeFormat = (contentArray) => {
-   let errorArray = []
-   for (let field of LOG_TIMESHEET_REQUIRED_FILEDS) {
-      let isValid = false
-      for (let line of contentArray) {
-         if (line.includes(`- ${field}:`)) {
-            isValid = true
-            break
-         }
-      }
-      if (!isValid) errorArray.push(field)
-   }
-   return errorArray
-}
-const extractLogTimeValue = (contentArray) => {
-   const contentObj = {}
-   for (let field of LOG_TIMESHEET_REQUIRED_FILEDS) {
-      for (let line of contentArray) {
-         if (line.includes(`- ${field}:`)) {
-            contentObj[field] = line.replace(`- ${field}:`, '').trim()
-            break
-         }
-      }
-   }
-   return contentObj
-}
-const validateFields = (contentObj) => {
-   let errorArray = []
-   const NORMAL_WORKING_TIME = 8
 
-   for (let [key, value] of Object.entries(contentObj))
-      if (value === '') errorArray.push(`Emmpty content at fields "${key}"\n`)
-   if (!contentObj['workingTime'] || contentObj['workingTime'] === '' || isNaN(parseFloat(contentObj['workingTime'])))
-      errorArray.push(`Invalid number in field "workingTime"\n`)
-   return errorArray
+const normalizeString = (str) => {
+  return (str || '').trim()
 }
-/* <task_name> 1h;
-   .+ any chracters
-   \s space
-   d+(\.\d*)?h  === 1h | 1.1h
-*/
 
-const VALID_TASK_FORMAT = /^.+\s\d+(\.\d*)?h$/
+const parseTimeSheetTask = (chunk) => {
+  const [note, meta] = (chunk || '').split(';')
+  const [timeRaw, type, name] = (meta || '').split(',')
+  const time = normalizeString(timeRaw)
+  const duration = parseDuration(time)
+  const task = {
+    note: normalizeString(note),
+    time: time,
+    duration: duration,
+    type: normalizeString(type),
+    name: normalizeString(name),
+  }
+  return task
+}
 
-const extractTasks = (noteStr) => {
-   const taskArray = noteStr.split(';').filter(item => VALID_TASK_FORMAT.test(item))
-   const trimTask = taskArray.map(item => item.trim())
-   const taskWithoutHourCharacter = trimTask.map(item => item.slice(0, -1))
-   return taskWithoutHourCharacter.map(item => ({ task: item.split(' ')[0], workingTime: parseFloat(item.split(' ')[1]) }))
+const parseTimeSheetSentence = (sentence) => {
+  const chunks = sentence.split(new RegExp('\\+', 'ig'))
+  const items = chunks
+    .filter((chunk) => chunk.trim())
+    .map((chunk) => parseTimeSheetTask(chunk))
+  return items
+}
+
+const parseDailyMessage = (message) => {
+  const [, dateRaw, yesterday, todayRaw, block] = message.split(
+    new RegExp('\\*daily|- yesterday:|- today:|- block:', 'ig'),
+  )
+  const dateStr = normalizeString(dateRaw)
+  const todayStr = normalizeString(todayRaw)
+  const date = chrono.parseDate(dateStr)
+  const tasks = parseTimeSheetSentence(todayStr)
+  const contentObj = {
+    date: dateStr,
+    timeStamp: date,
+    yesterday: normalizeString(yesterday),
+    today: todayStr,
+    block: normalizeString(block),
+    tasks,
+  }
+
+  return contentObj
+}
+
+const logTimeSheetFromDaily = async ({ content, emailAddress, url, token }) => {
+  const data = parseDailyMessage(content)
+
+  for (const task of data.tasks) {
+    const timesheetPayload = {
+      emailAddress,
+      note: task.note,
+    }
+    try {
+      await axios.post(url, timesheetPayload, {
+        headers: {
+          securitycode: token,
+        },
+      })
+    } catch (e) {
+      // TODO: return result
+      console.log(e)
+    }
+  }
 }
 
 const debug = (...messages) => {
-   if (DEBUG) console.log(...messages)
+  if (DEBUG) console.log(...messages)
 }
-const setDebug = () => DEBUG = true
+const setDebug = () => (DEBUG = true)
 const getDebug = () => DEBUG
 
 module.exports = {
-   checkLogTimeFormat,
-   extractLogTimeValue,
-   validateFields,
-   debug,
-   setDebug,
-   getDebug,
-   extractTasks,
-   TIMESHEET_MESSAGE_HELP,
+  parseTimeSheetTask,
+  parseTimeSheetSentence,
+  parseDailyMessage,
+  logTimeSheetFromDaily,
+  debug,
+  setDebug,
+  getDebug,
 }
