9805d6d4990ad74e33b8684644c81df12366d641
SinhHai <hai.nguyensinh.ncc@gmail.com>
2022-10-05 00:01:02 +0700

    build command workout and report workout

---
+++ commands/komu/workout.js
@@ -0,0 +1,192 @@
+const { MessageActionRow, MessageButton, MessageEmbed } = require('discord.js');
+const workoutData = require('../../models/workoutData');
+const { sendErrorToDevTest } = require('../../util/komubotrest');
+
+const monthSupport = [
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '10',
+  '11',
+  '12',
+  'JAN',
+  'FEB',
+  'MAR',
+  'APR',
+  'MAY',
+  'JUN',
+  'JUL',
+  'AUG',
+  'SEP',
+  'OCT',
+  'NOV',
+  'DEC',
+];
+
+module.exports = {
+  name: 'workout',
+  description: 'workout daily',
+  cat: 'komu',
+  async execute(message, args, client) {
+    try {
+      const authorId = message.author.id;
+      if (args[0] === 'summary') {
+        if (monthSupport.includes(args[1].toUpperCase())) {
+          const date = new Date();
+          let dateFormat;
+          const year = date.getFullYear();
+          if (args[1].length > 2) {
+            dateFormat = new Date(`${args[1]} ${year}`);
+          } else {
+            dateFormat = new Date(year, +args[1] - 1);
+          }
+          const y = dateFormat.getFullYear();
+          const m = dateFormat.getMonth();
+          const firstDay = new Date(y, m, 1);
+          const lastDay = new Date(y, m + 1, 0);
+
+          const userCheckWorkout = await workoutData.aggregate([
+            {
+              $match: {
+                channelId: message.channelId,
+                createdTimestamp: {
+                  $gte: firstDay.getTime(),
+                  $lte: lastDay.getTime(),
+                },
+                status: false,
+              },
+            },
+            {
+              $group: {
+                _id: '$userId',
+                total: { $sum: 1 },
+                email: { $first: '$email' },
+                channelId: { $first: '$channelId' },
+                userId: { $first: '$userId' },
+              },
+            },
+            {
+              $project: {
+                _id: 0,
+                total: 1,
+                email: 1,
+                channelId: 1,
+                userId: 1,
+              },
+            },
+          ]);
+
+          let mess;
+          if (!userCheckWorkout) {
+            return;
+          } else if (
+            Array.isArray(userCheckWorkout) &&
+            userCheckWorkout.length === 0
+          ) {
+            mess = '```' + 'Không có ai vi phạm trong tháng' + '```';
+            return message.reply(mess).catch((err) => {
+              sendErrorToDevTest(client, m, err);
+            });
+          } else {
+            for (
+              let i = 0;
+              i <= Math.ceil(userCheckWorkout.length / 50);
+              i += 1
+            ) {
+              if (userCheckWorkout.slice(i * 50, (i + 1) * 50).length === 0) {
+                break;
+              }
+              mess = userCheckWorkout
+                .slice(i * 50, (i + 1) * 50)
+                .map((item) => `${item.email} (${item.total})`)
+                .join('\n');
+              const Embed = new MessageEmbed()
+                .setTitle('Những người không workout trong tháng')
+                .setColor('RED')
+                .setDescription(`${mess}`);
+              await message.reply({ embeds: [Embed] }).catch((err) => {
+                sendErrorToDevTest(client, authorId, err);
+              });
+            }
+          }
+        }
+      } else if (args[0] === 'help') {
+        return message.channel
+          .reply('```' + '*workout month' + '\n' + '*workout' + '```')
+          .catch(console.error);
+      } else {
+        const links = [];
+        if (message.attachments && message.attachments.first()) {
+          message.attachments.forEach((attachment) => {
+            try {
+              const imageLink = attachment.proxyURL;
+              links.push(imageLink);
+            } catch (error) {
+              console.error(error);
+            }
+          });
+          if (links.length > 0) {
+            const workout = await new workoutData({
+              userId: message.author.id,
+              email:
+                message.member != null || message.member != undefined
+                  ? message.member.displayName
+                  : message.author.username,
+              createdTimestamp: Date.now(),
+              attachment: true,
+              status: false,
+              channelId: message.channel.id,
+            })
+              .save()
+              .catch((err) => console.log(err));
+
+            const row = new MessageActionRow().addComponents(
+              new MessageButton()
+                .setCustomId(
+                  'workout_approve#' +
+                    workout.email +
+                    '#' +
+                    workout._id +
+                    '#' +
+                    workout.channelId +
+                    '#' +
+                    message.author.id
+                )
+                .setLabel('APPROVE')
+                .setStyle('PRIMARY'),
+              new MessageButton()
+                .setCustomId(
+                  'workout_reject#' +
+                    workout.email +
+                    '#' +
+                    workout._id +
+                    '#' +
+                    workout.channelId +
+                    '#' +
+                    message.author.id
+                )
+                .setLabel('REJECT')
+                .setStyle('DANGER')
+            );
+
+            message.reply({
+              content: '`✅` workout daily saved.',
+              ephemeral: true,
+              components: [row],
+            });
+          }
+        } else {
+          message.reply('Vui lòng gửi tệp đính kèm').catch(console.error);
+        }
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  },
+};
---
+++ util/workout.js
@@ -0,0 +1,156 @@
+const workoutData = require('../models/workoutData');
+const userData = require('../models/userData');
+const { MessageButton, MessageActionRow, MessageEmbed } = require('discord.js');
+const { sendErrorToDevTest } = require('./komubotrest');
+
+const workout = async (interaction, client) => {
+  const arrIds = interaction.customId.split('#');
+  const labelImageId = arrIds.length > 1 ? arrIds[4] : '';
+  const labelImageEmail = arrIds.length > 1 ? arrIds[1] : '';
+  const workourid = arrIds[2];
+
+  const authorId = interaction.message.author.id;
+  const checkRole = await userData.find({
+    id: interaction.user.id,
+    deactive: { $ne: true },
+    $or: [{ roles_discord: { $all: ['HR'] } }],
+  });
+  if (
+    checkRole.length > 0 ||
+    interaction.user.id === '921261168088190997' ||
+    interaction.user.id === '868040521136873503' ||
+    interaction.user.id === '922148445626716182'
+  ) {
+    if (
+      arrIds.length > 2 &&
+      arrIds[0] == 'workout_reject' &&
+      // labelImageId == interaction.user.id &&
+      authorId == client.user.id
+    ) {
+      const workoutDb = await workoutData
+        .findOne({ _id: workourid })
+        .catch(console.error);
+      if (!workoutDb) {
+        interaction
+          .reply({ content: 'No workout found', ephemeral: true })
+          .catch((err) => {
+            sendErrorToDevTest(client, authorId, err);
+          });
+        return;
+      }
+
+      if (!workoutDb.status) {
+        interaction
+          .reply({
+            content: 'You have already complained.',
+            ephemeral: true,
+          })
+          .catch((err) => {
+            sendErrorToDevTest(client, authorId, err);
+          });
+        return;
+      }
+      const userdb = await userData
+        .findOne({ id: labelImageId, deactive: { $ne: true } })
+        .catch(console.error);
+      if (!userdb) {
+        return interaction
+          .reply({ content: '`User is not valid`', ephemeral: true })
+          .catch(console.error);
+      }
+      const message = `${interaction.user.username} just rejected workout reject from ${labelImageEmail}`;
+
+      await client.channels.cache
+        .get(arrIds[3])
+        .send(message)
+        .catch(console.error);
+      await workoutData
+        .updateOne(
+          { _id: workourid },
+          {
+            status: false,
+          }
+        )
+        .catch(console.error);
+
+      await interaction
+        .reply({
+          content: `You just rejected workout reject for ${labelImageEmail}`,
+          ephemeral: true,
+        })
+        .catch(console.error);
+      return;
+    } else if (
+      arrIds.length > 2 &&
+      arrIds[0] == 'workout_approve' &&
+      // labelImageId == interaction.user.id &&
+      authorId == client.user.id
+    ) {
+      if (arrIds.length === 5) {
+        const workoutDb = await workoutData
+          .findOne({ _id: workourid })
+          .catch(console.error);
+        if (!workoutDb) {
+          interaction
+            .reply({ content: 'No workout found', ephemeral: true })
+            .catch((err) => {
+              sendErrorToDevTest(client, authorId, err);
+            });
+          return;
+        }
+
+        if (workoutDb.status) {
+          interaction
+            .reply({
+              content: 'You have already complained.',
+              ephemeral: true,
+            })
+            .catch((err) => {
+              sendErrorToDevTest(client, authorId, err);
+            });
+          return;
+        }
+        const userdb = await userData
+          .findOne({ id: labelImageId, deactive: { $ne: true } })
+          .catch(console.error);
+        if (!userdb) {
+          return interaction
+            .reply({ content: '`User is not valid`', ephemeral: true })
+            .catch(console.error);
+        }
+        const message = `${interaction.user.username} just confirmed workout complain from ${labelImageEmail}`;
+        await workoutData
+          .updateOne(
+            { _id: workourid },
+            {
+              status: true,
+            }
+          )
+          .catch(console.error);
+        await client.channels.cache
+          .get(arrIds[3])
+          .send(message)
+          .catch(console.error);
+        await interaction
+          .reply({
+            content: `You just confirmed workout complain for ${labelImageEmail}`,
+            ephemeral: true,
+          })
+          .catch(console.error);
+      }
+      return;
+    }
+  } else {
+    interaction
+      .reply({
+        content: 'You do not have permission to execute this workout',
+        ephemeral: true,
+      })
+      .catch((err) => {
+        sendErrorToDevTest(client, authorId, err);
+      });
+    return;
+  }
+};
+
+module.exports = workout;
